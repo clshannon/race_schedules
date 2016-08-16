7a60fd7dd5882ca38caad37eabf4a3e96534c50c
Chris Shannon <chris@readymonster.com>
2016-08-16 15:09:37 -0400

    RemovedInDjango110Warning Fix Commit
    raceschedules.raceschedules.api.urls
    -Changed urlpatterns to a list to prevent warning message on runserver

--- raceschedules/api/urls.py
+++ raceschedules/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import *
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'', include('raceschedules.api.v1.urls', namespace='default')),
     url(r'^v1/', include('raceschedules.api.v1.urls', namespace='v1')),
-)
+]
