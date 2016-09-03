79cf93e3334f5f795e8d2c71d691d8e6e2e9b830
chrisshannon <chris@readymonster.com>
2016-09-03 16:15:23 -0400

    Config Change Commit
    -Changed production database setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -125,7 +125,12 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 # DATABASE CONFIGURATION
 # ------------------------------------------------------------------------------
 # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
-DATABASES['default'] = env.db('DATABASE_URL')
+#DATABASES['default'] = env.db('DATABASE_URL')
+DATABASES = {
+    'default': {
+         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+    },
+}
 
 # CACHING
 # ------------------------------------------------------------------------------
